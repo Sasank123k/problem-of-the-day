f40167f9dc963f9933e008875b548a531016bafc
Sasank123k <pothulanaga.sasank2021@vitstudent.ac.in>
2021-12-23 05:09:15 +0530

    added the potd

---
+++ 22-12-2021.py
@@ -0,0 +1,29 @@
+"""
+Stepped Strings
+Two equal length strings S1 and S2 are said to be stepped strings if the difference between the letter position in English alphabet of non space characters is ‘i’ where ‘i’ is the position of the character in the strings excluding spaces in the strings. Letter position of character ‘a’ in English alphabet is 1, letter position of character ‘b’ is 2 and so on.
+
+ 
+
+Given two strings S1 and S2, write a C program to check if S1 and S2 are stepped strings. If the strings are of different length then print  "Length different" and if strings contain non alphabets then print "Strings contain non alphabets" and if strings differ in the position of space then print  "Strings differ in space" and print  "Strings are not stepped" when valid strings are given and they differ in the step value
+
+ 
+
+Input Format
+
+First line contains the string, s
+
+Output Format
+
+Print either Strings are stepped or Length different or Strings differ in space or Strings are not stepped or Strings contain non alphabets
+
+print(lst)
+Input
+abcd defg
+bdfh ikmo
+Expected output
+Strings are stepped
+Your Program Output
+[1, 1, 1, -68, 68, 1, 1, 1]
+
+
+"""
